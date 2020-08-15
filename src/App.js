f8c05170d15da76684e0f27ddb05b34c3efbf16d
Rettus code <michael.rettusii@gmail.com>
2020-08-14 21:49:12 -0700

    installed dependencies needed so far, and created responsive iframe, needs tweeking for various platforms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Jitsi from "./components/Jitsi/jitsi";
+//import About from "./pages/About";
+//import Search from "./pages/Search";
+//import Menu from "./components/Menu";
+//import Footer from "./components/Footer";
+//import Wrapper from "./components/Wrapper";
+import 'antd/dist/antd.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return  <div>
+    <Jitsi />
+    
+  
+  </div>;
 }
+ 
 
 export default App;
