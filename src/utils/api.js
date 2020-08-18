41a514e5ef51433705f458d5cadbef2d06e892de
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-18 10:26:29 -0700

    added userLogin route frontend still works with backend.

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,11 +3,15 @@ const axios = require("axios")
 const API = {
     //User Related API calls
     saveUser: function(userData) {
-        return axios.post("https://localhost:8080/api/users", userData)
+        return axios.post("http://localhost:8080/api/users/signup", userData)
+    },
+    // TODO: next test, get user by ID then display user page.  
+        getUser: function(id) {
+        return axios.get(`http://localhost:8080/api/users/${id}`)
+    },
+    userLogin: function(userData) {
+        return axios.post("http://localhost:8080/api/users/login", userData)
     }
-
-
-    
     //Hall Related API calls
 
 
