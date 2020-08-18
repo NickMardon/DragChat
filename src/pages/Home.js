9f0b46aaff789f5f165e41e3c01d437c83114e90
Nick Mardon <nickmardondigital@gmail.com>
2020-08-18 11:12:30 -0700

    create url copy to clipboard input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HallForm from '../components/hallform/hallform.component';
 import Jitsi from '../components/Jitsi/jitsi';
+import URLCopyInput from "../components/URLCopyInput/URLCopyInput";
 
 export default function Home() {
     return (
         <div>
+            <URLCopyInput />
             <Jitsi />
             <HallForm />
         </div>
