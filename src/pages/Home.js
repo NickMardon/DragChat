7974ef9bccb2620644b64c0cb8f9e9957dd53222
Nick Mardon <nickmardondigital@gmail.com>
2020-08-18 11:34:57 -0700

    added webcam component to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import HallForm from '../components/hallform/hallform.component';
 import SignIn from '../components/SignIn/SignIn';
 import Jitsi from '../components/Jitsi/jitsi';
 import URLCopyInput from "../components/URLCopyInput/URLCopyInput";
+import Webcam from "../components/Webcam/Webcam";
 
 export default function Home() {
     return (
         <div>
             <URLCopyInput />
-            <Jitsi />
+            {/* <Jitsi /> */}
+            <Webcam />
             <SignIn />
             <HallForm />
         </div>
