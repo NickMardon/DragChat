a83f28c18f83f7e4251b5e00436928f83580e8ca
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-19 01:31:25 -0700

    string literal needs bactics, evan.

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,19 +4,19 @@ const urlPrefix = "https://dropinsamine.herokuapp.com"
 const API = {
     //User Related API calls
     saveUser: function(userData) {
-        return axios.post("${urlPrefix}/api/users/signup", userData)
+        return axios.post(`${urlPrefix}/api/users/signup`, userData)
     },
     // TODO: put in use on user page  
     //  getUser: function(id) {
     //     return axios.get(`${urlPrefix}/api/users/${id}`)
     // },
     userLogin: function(userData) {
-        return axios.post("${urlPrefix}/api/users/login", userData)
+        return axios.post(`${urlPrefix}/api/users/login`, userData)
     },
 // ------------------------------------------
     //Hall Related API calls
     createHall: function(hallData) {
-        return axios.post("${urlPrefix}/api/halls/create", hallData, { withCredentials: true })
+        return axios.post(`${urlPrefix}/api/halls/create`, hallData, { withCredentials: true })
     },
 
     //all the halls that belong to the currently logged in user
