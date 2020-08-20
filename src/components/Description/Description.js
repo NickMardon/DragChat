00d7288f2f20aab30f17db248cff41781579d77e
Rettus code <michael.rettusii@gmail.com>
2020-08-20 15:22:55 -0700

    home page alignment fixes

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./index.css";
 import "./backgroundPhoto.jpeg";
-
 export default function Description() {
     return (
         <div className="jumbotron">
             <h1>Dropin Video Conferences</h1>
-            <h3>Broadside heave down salmagundi belaying pin yawl ballast bucko parrel ho doubloon. Hornswaggle list Nelsons folly Jolly Roger deadlights lugger long boat reef sails hail-shot Spanish Main. Cable list Pirate Round overhaul marooned tackle provost Admiral of the Black haul wind rutters.</h3>
+            
+            <h3>Broadside heave down salmagundi belaying pin yawl 
+                ballast bucko parrel ho doubloon. Hornswaggle list Nelsons 
+                folly Jolly Roger deadlights lugger long boat reef sails 
+                hail-shot Spanish Main. Cable list Pirate Round overhaul 
+                marooned tackle provost Admiral of the Black haul wind rutters.
+                Broadside heave down salmagundi belaying pin yawl 
+                ballast bucko parrel ho doubloon. Hornswaggle list Nelsons 
+                folly Jolly Roger deadlights lugger long boat reef sails 
+                hail-shot Spanish Main. Cable list Pirate Round overhaul 
+                marooned tackle provost Admiral of the Black haul wind rutters.</h3>
         </div>
     )
 }
