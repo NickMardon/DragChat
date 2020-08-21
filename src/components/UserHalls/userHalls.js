e2ca4911fedaabff1c0e41542b255505a78b81ba
Rettus code <michael.rettusii@gmail.com>
2020-08-21 14:18:48 -0700

    multiple style adjustments, and user hall functioning

--- src/components/UserHalls/userHalls.js
+++ src/components/UserHalls/userHalls.js
@@ -3,16 +3,19 @@ import { Card } from 'antd';
 import Button from "../Button/Button"
 
 export default function userHall(props) {
-
-        return ( 
-            <div>
-                <Card title="User's Halls" style={{ width: "44%", margin: "2%" }}>
-                    <p>Hall name {props.name}</p><Button /><Button />
-                    <p>Number of rooms {props.hall_size}</p><Button /><Button />
-                    <p>password {props.password}</p><Button /><Button />
-                    <p>Hall theme {props.theme_id}</p><Button /><Button />
-                </Card>
-            </div>
-        )
+    console.log(props);
+    return (
+        
+    <div>
+        {props.props&&props.props.name?
+        (<Card title= {props.props.name} style={{ width: "44%", margin: "2%" }  }><Button /><Button />
+            <p>Hall name {props.props.name}</p>
+            <p>Number of rooms {props.props.hallSize}</p><Button /><Button />
+            <p>password {props.props.password}</p><Button /><Button />
+            <p>Hall theme {props.props.description}</p><Button /><Button />
+        </Card>):<h1>this is no bueno</h1>}
+    </div>
+    
+    )
 }
 
