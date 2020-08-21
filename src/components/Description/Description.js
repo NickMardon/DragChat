16f480f34652fa7b8e41366a189aca7a2f4979a6
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-21 10:23:04 -0700

    fixed local issues, pulling

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./index.css";
 import "./backgroundPhoto.jpeg";
-<<<<<<< HEAD
 import AccountForm from "../AccountForm/AccountForm.component";
 import SignIn from "../SignIn/SignIn";
 import CreateHall from "../CreateHall/createHall";
@@ -16,23 +15,6 @@ export default function Description(props) {
       {!props.isLoggedIn && <AccountForm setCurrentUser={props.setCurrentUser} />}
       {props.isLoggedIn && <CreateHall />}
       </div>
-=======
-export default function Description() {
-    return (
-        <div className="jumbotron">
-            <h1>Dropin Video Conferences</h1>
-            
-            <h3>Broadside heave down salmagundi belaying pin yawl 
-                ballast bucko parrel ho doubloon. Hornswaggle list Nelsons 
-                folly Jolly Roger deadlights lugger long boat reef sails 
-                hail-shot Spanish Main. Cable list Pirate Round overhaul 
-                marooned tackle provost Admiral of the Black haul wind rutters.
-                Broadside heave down salmagundi belaying pin yawl 
-                ballast bucko parrel ho doubloon. Hornswaggle list Nelsons 
-                folly Jolly Roger deadlights lugger long boat reef sails 
-                hail-shot Spanish Main. Cable list Pirate Round overhaul 
-                marooned tackle provost Admiral of the Black haul wind rutters.</h3>
->>>>>>> dev
         </div>
     )
 }
