a402f6c6cb26e060daf2b6daab6cd12e372eb18b
Nick Mardon <nickmardondigital@gmail.com>
2020-08-22 15:52:09 -0700

    added homesidecard and footer components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+function Footer() {
+    return (
+        <div>
+            <footer class="footerBox">
+                <div class="footerContainer">
+                    <p id="footerText">Copyright &#169; 2020 Videezy Inc.</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
