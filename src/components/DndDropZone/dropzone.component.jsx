9361b19c0abb9d685938c14d8381c0c5147c26a9
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-23 13:18:00 -0700

    adding context

--- src/components/DndDropZone/dropzone.component.jsx
+++ src/components/DndDropZone/dropzone.component.jsx
@@ -1,27 +1,48 @@
-import React from "react";
-import {Col } from "antd";
-import "./style.css"
+import React, { useContext } from "react";
+import { useDrop } from "react-dnd";
+import { ItemTypes } from "../../utils/items";
+// TODO: IMPORT DND CONTEXT AS EXPORTED FROM DNDFRAME
+import { Col } from "antd";
+import "./style.css";
 
 export default function DndDropZone(props) {
+
+  //     const { markAsConnected } = useContext(RoomContext)
+
+  //     const[{isOver}, drop] = useDrop({
+  //     accept: ItemTypes.BADGE,
+  //     drop: (item, monitor) => markAsConnected(item.id),
+  //     collect: monitor => ({
+  //         isOver: !!monitor.isOver()
+  //     })
+  // })
+
   return (
-      <Col xs={{span: 12}}>
-
-     
-    <div className="dropArea">
-      {
-               props.name&&props.id
-               ? (
-            <>
-        
+    <Col xs={{ span: 12 }}>
+      <div
+        className={`dropArea`}
+        // TODO: className={`dropArea + ${isOver ? 'isOver':''}`}
+        //  TODO:ref={drop}
+      >
+        {props.name && props.id ? (
+          <>
             <br></br>
-            <span>{props.id}</span></>
+            <br></br>
+            <span>{props.id}</span>
+            <br></br>
+            <br></br>
+          </>
+        ) : (
+          <span>???</span>
+        )}
 
-               ) : (
-                   <span>???</span>
-               )
-           } 
 
-    </div>
+
+        {props.children}
+
+
+
+      </div>
     </Col>
   );
 }
