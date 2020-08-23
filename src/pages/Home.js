9297fa9cb6ef6ca3224ab7f1b0c58a770599f30d
Nick Mardon <nickmardondigital@gmail.com>
2020-08-22 23:42:28 -0700

    added user review cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Description from "../components/Description/Description";
 import Card from "../components/Card/Card";
 import Footer from "../components/Footer/Footer";
 import { Row } from 'antd';
+import Review from "../components/Reviews/Reviews";
 export default function Home(props) {
   const [isLoggedIn,] = useState(props.currentUser?.id? true : false, [props.currentUser])
 
@@ -19,6 +20,17 @@ export default function Home(props) {
       <Row>
       <Webcam />
       <Card />
+      </Row>
+      <Row>
+        <Review 
+        review="This app is really good" reviewer="Mike Tyson"
+        />
+        <Review
+           review="Hella tight app dude" reviewer="The Big Lebowski"
+        />
+        <Review 
+           review="Videezy is the only video app for yeezy" reviewer="Kanye West"
+        />
       </Row>
       <Footer />
     </div>
